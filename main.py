f833da59dabc35c6a424a2b57fd4af31dfca0449
Gustavo Corrêa <ecfgustavo@gmail.com>
2024-05-17 01:48:57 -0300

    Update

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from flask import render_template
+from flask import url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('padrao.html',
+    return render_template('home.html',
                            docTitle = 'Liga PHSC')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html',
+                           docTitle = 'Liga PHSC - Sobre')
 
 @app.route('/contato')
 def contato():
