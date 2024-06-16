1597949964a46abc778806447cd082a4eff9dfd8
Gustavo Corrêa <ecfgustavo@gmail.com>
2024-06-16 17:54:41 -0300

    Update

--- main.py
+++ main.py
@@ -4,28 +4,28 @@ from flask import url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['GET'])
 def home():
     return render_template('padrao.html',
                            docTitle = 'Liga PHSC')
 
-@app.route('/sobre')
+@app.route('/sobre', methods = ['GET'])
 def sobre():
     return render_template('sobre.html',
                            docTitle = 'Liga PHSC - Sobre')
 
-@app.route('/contato')
+@app.route('/contato', methods = ['GET'])
 def contato():
     return render_template('contato.html',
                            docTitle = 'Liga PHSC - Contato')
 
-@app.route('/teste')
+@app.route('/teste', methods = ['GET'])
 def teste():
     return render_template('teste_bootstrap.html',
                            docTitle = 'Liga PHSC - Testes')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host = '0.0.0.0')
 
 ''' --- PARA HOSPEDAR NO HEROKU ---
 
